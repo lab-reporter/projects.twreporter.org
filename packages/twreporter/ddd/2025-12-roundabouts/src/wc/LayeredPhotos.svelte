d788b5647543858f516320f01e19de99595b106f
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2025-11-26 11:21:09 +0800

    feat(2025-12-roundabouts): add layered photos component

--- packages/twreporter/ddd/2025-12-roundabouts/src/wc/LayeredPhotos.svelte
+++ packages/twreporter/ddd/2025-12-roundabouts/src/wc/LayeredPhotos.svelte
@@ -1,10 +1,55 @@
+<script lang="ts">
+  type Layer = { legend?: string; name: string; src: string }
+  type LayerState = Layer & { show: boolean }
+
+  let { name, base, layers }: { name: string; base: string; layers: Layer[] } =
+    $props()
+
+  let layerState = $state(
+    layers.map((l, index) => ({ ...l, show: index === 0 ? true : false }))
+  )
+
+  const updateLayer = (name: string, updatedLayer: LayerState) => {
+    layerState = layerState.map((layer) =>
+      layer.name === name ? updatedLayer : { ...layer, show: false }
+    )
+  }
+</script>
+
 <link
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/tailwindcss-preflight@1.0.1/preflight.min.css"
   crossorigin="anonymous"
 />
 
-<div>Hello World</div>
+<div class="container">
+  <div class="controls">
+    {#each layerState as layer}
+      <button
+        title={layer.name}
+        class:active={layer.show}
+        onmouseenter={() =>
+          updateLayer(layer.name, {
+            ...layer,
+            show: true,
+          })}
+      >
+        {#if layer.legend}
+          <img src={layer.legend} alt={layer.name} class="legend" />
+        {/if}
+        {layer.name}</button
+      >
+    {/each}
+  </div>
+  <div class="images">
+    <img src={base} alt={name} />
+    <div class="layers">
+      {#each layerState as layer}
+        <img src={layer.src} alt={layer.name} class:show={layer.show} />
+      {/each}
+    </div>
+  </div>
+</div>
 
 <style>
   * {
@@ -12,5 +57,56 @@
     color: var(--tr-text);
     font-family: 'Roboto Slab', 'Noto Sans TC', sans-serif;
     text-align: left !important;
+  }
+
+  .container {
+    max-width: 730px;
+    position: relative;
+  }
+
+  .images {
+    width: 100%;
+    position: relative;
+  }
+
+  .layers {
+    width: 100%;
+  }
+
+  .layers img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+  }
+
+  .layers .show {
+    opacity: 1;
+  }
+
+  .controls {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background: white;
+  }
+
+  .controls button {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 10px 10px;
+  }
+
+  .controls .active {
+    font-weight: bold;
+  }
+
+  .controls .legend {
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
   }
 </style>
