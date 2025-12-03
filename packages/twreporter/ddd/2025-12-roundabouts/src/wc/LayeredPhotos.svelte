57883bcbda804e7a911649809ebbef512a1f6a58
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2025-12-03 11:44:39 +0800

    feat(2025-12-roundabouts): add dynamic groups

--- packages/twreporter/ddd/2025-12-roundabouts/src/wc/LayeredPhotos.svelte
+++ packages/twreporter/ddd/2025-12-roundabouts/src/wc/LayeredPhotos.svelte
@@ -1,258 +1,138 @@
 <script lang="ts">
-  type Layer = { legend?: string; name: string; src: string }
-  type LayerState = Layer & { show: boolean }
+    type Layer = { legend?: string; name: string; src: string };
+    type LayerState = Layer & { show: boolean };
 
-  let {
-    name,
-    base,
-    layers,
-    footnotes,
-  }: { name: string; base: string; layers: Layer[]; footnotes: string[] } =
-    $props()
+    let { base, layers }: { base: string; layers: Layer[] } = $props();
 
-  let layerState = $state(layers.map((l, index) => ({ ...l, show: true })))
+    let layerState = $state(layers.map((l, index) => ({ ...l, show: true })));
 
-  let lockedState = $state(true)
+    let lockedState = $state(true);
 
-  const updateLayer = (name: string, updatedLayer: LayerState) => {
-    layerState = layerState.map((layer) =>
-      layer.name === name ? updatedLayer : { ...layer, show: false }
-    )
-  }
+    const updateLayer = (name: string, updatedLayer: LayerState) => {
+        layerState = layerState.map((layer) =>
+            layer.name === name ? updatedLayer : { ...layer, show: false },
+        );
+    };
 
-  const activateLayer = (layer: LayerState) => {
-    updateLayer(layer.name, { ...layer, show: true })
-  }
+    const activateLayer = (layer: LayerState) => {
+        updateLayer(layer.name, { ...layer, show: true });
+    };
 </script>
 
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/tailwindcss-preflight@1.0.1/preflight.min.css"
-  crossorigin="anonymous"
-/>
-
-<div class="container">
-  <div class="header"><h1>{name}</h1></div>
-  <div class="controls">
+<div class="controls">
     <button
-      class:active={layerState.every((layer) => layer.show)}
-      class:all={layerState.every((layer) => layer.show)}
-      onclick={() => {
-        lockedState = true
-        layerState = layerState.map((layer) => ({ ...layer, show: true }))
-      }}
-      class="full"
+        class:active={layerState.every((layer) => layer.show)}
+        class:all={layerState.every((layer) => layer.show)}
+        onclick={() => {
+            lockedState = true;
+            layerState = layerState.map((layer) => ({
+                ...layer,
+                show: true,
+            }));
+        }}
+        class="full"
     >
-      顯示所有事故
+        顯示所有事故
     </button>
     <div class="indv">
-      {#each layerState as layer}
-        <button
-          title={layer.name}
-          class:active={layer.show}
-          class:all={layerState.every((layer) => layer.show)}
-          onclick={() => {
-            activateLayer(layer)
-            lockedState = false
-          }}
-          onmouseenter={() => {
-            if (!lockedState) activateLayer(layer)
-          }}
-        >
-          {#if layer.legend}
-            <img src={layer.legend} alt={layer.name} class="legend" />
-          {/if}
-          {layer.name}</button
-        >
-      {/each}
+        {#each layerState as layer}
+            <button
+                title={layer.name}
+                class:active={layer.show}
+                class:all={layerState.every((layer) => layer.show)}
+                onclick={() => {
+                    activateLayer(layer);
+                    lockedState = false;
+                }}
+                onmouseenter={() => {
+                    if (!lockedState) activateLayer(layer);
+                }}
+            >
+                {#if layer.legend}
+                    <img src={layer.legend} alt={layer.name} class="legend" />
+                {/if}
+                {layer.name}</button
+            >
+        {/each}
     </div>
-  </div>
-  <div class="images">
-    <img src={base} alt={name} />
+</div>
+<div class="images">
+    <img src={base} alt={base} />
     <div class="layers">
-      {#each layerState as layer}
-        <img src={layer.src} alt={layer.name} class:show={layer.show} />
-      {/each}
+        {#each layerState as layer}
+            <img src={layer.src} alt={layer.name} class:show={layer.show} />
+        {/each}
     </div>
-  </div>
-  <div class="footer">
-    <div class="footnotes">
-      {#each footnotes as footnote}
-        <p>{footnote}</p>
-      {/each}
-    </div>
-    <img
-      src="https://projects.twreporter.org/twreporter/ddd/2025-0823-vote/assets/logo-black.png"
-      class="logo"
-      alt="報導者 The Reporter"
-    />
-  </div>
 </div>
 
 <style>
-  * {
-    --tr-text: #404040;
-    color: var(--tr-text);
-    font-family: 'Roboto Slab', 'Noto Sans TC', sans-serif;
-    text-align: left !important;
-  }
-
-  .container {
-    max-width: 730px;
-    position: relative;
-    padding: 8px 10px;
-    background: #f1f1f1;
-    --btn-size: 10px;
-    border-top: #e2e2e2 1px solid;
-    border-bottom: #e2e2e2 1px solid;
-  }
-
-  @media (min-width: 500px) {
-    .container {
-      padding: 10px 10px;
+    .images {
+        width: 100%;
+        position: relative;
     }
-  }
 
-  .header {
-    padding: 5px 0 0;
-  }
-
-  @media (min-width: 500px) {
-    .header h1 {
-      padding: 5px 0 15px;
+    .layers {
+        width: 100%;
     }
-  }
 
-  .header h1 {
-    font-size: 24px;
-    font-weight: bold;
-    padding: 5px 0 10px;
-  }
-
-  @media (min-width: 500px) {
-    .header h1 {
-      font-size: 28px;
+    .layers img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        opacity: 0;
     }
-  }
 
-  .images {
-    width: 100%;
-    position: relative;
-  }
-
-  .layers {
-    width: 100%;
-  }
-
-  .layers img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    opacity: 0;
-  }
-
-  .layers .show {
-    opacity: 1;
-  }
-
-  .controls {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-    gap: 5px;
-  }
-
-  .controls .indv {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  .controls button {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 4px 0;
-    background: white;
-    border-radius: 5px 5px 0 0;
-    font-size: var(--btn-size);
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    opacity: 0.5;
-    letter-spacing: 0.7px;
-  }
-
-  .controls .full {
-    border-radius: 5px;
-  }
-
-  @media (min-width: 500px) {
-    .container {
-      --btn-size: 14px;
+    .layers .show {
+        opacity: 1;
     }
-  }
 
-  @media (min-width: 670px) {
-    .container {
-      --btn-size: 16px;
+    .controls {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        gap: 5px;
     }
-  }
 
-  .controls .active {
-    opacity: 1;
-    font-weight: 600;
-  }
-
-  .controls .all {
-    opacity: 1;
-    font-weight: 400;
-  }
-
-  .controls .legend {
-    width: var(--btn-size);
-    aspect-ratio: 1/1;
-    margin-right: 5px;
-  }
-
-  .footer {
-    padding: 10px 0 10px 0;
-    display: flex;
-    align-items: end;
-    justify-content: space-between;
-    --footer-scale: 1;
-    --footer-logo-scale: 1.25;
-  }
-
-  @media (min-width: 500px) {
-    .footer {
-      padding: 15px 0 10px 0;
-      --footer-scale: 1.6;
-      --footer-logo-scale: 2;
+    .controls .indv {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
     }
-  }
 
-  .footnotes {
-    display: flex;
-    flex-direction: column;
-    gap: 2px;
-  }
-
-  @media (min-width: 500px) {
-    .footnotes {
-      gap: 5px;
+    .controls button {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 4px 0;
+        background: white;
+        border-radius: 5px 5px 0 0;
+        font-size: var(--btn-size);
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        opacity: 0.5;
+        letter-spacing: 0.7px;
     }
-  }
 
-  .footer p {
-    color: #acacac;
-    font-size: calc(10px * var(--footer-scale));
-  }
+    .controls .full {
+        border-radius: 5px;
+    }
 
-  .footer .logo {
-    width: calc(14.5px * var(--footer-logo-scale));
-    height: calc(15.5px * var(--footer-logo-scale));
-  }
+    .controls .active {
+        opacity: 1;
+        font-weight: 600;
+    }
+
+    .controls .all {
+        opacity: 1;
+        font-weight: 400;
+    }
+
+    .controls .legend {
+        width: var(--btn-size);
+        aspect-ratio: 1/1;
+        margin-right: 5px;
+    }
 </style>
--- packages/twreporter/ddd/2025-12-roundabouts/src/wc/LayeredPhotos.wc.svelte
+++ packages/twreporter/ddd/2025-12-roundabouts/src/wc/LayeredPhotos.wc.svelte
@@ -1,23 +1,210 @@
-<svelte:options customElement={{ tag: 'twreporter-layered-photos' }} />
+<!-- svelte-ignore custom_element_props_identifier -->
+<svelte:options customElement={{ tag: "twreporter-layered-photos" }} />
 
 <script lang="ts">
-  import LayeredPhotos from './LayeredPhotos.svelte'
+    import LayeredPhotos from "./LayeredPhotos.svelte";
 
-  let {
-    name,
-    base,
-    layers,
-    footnotes,
-  }: { name: string; base: string; layers: string; footnotes: string } =
-    $props()
+    let {
+        name,
+        footnotes: inputFootnotes,
+        ...props // We use non-destructured props to support dynamic props with group ids in them.
+    }: { name: string; footnotes: string } & (
+        | { base: string; layers: string }
+        | {
+              groups: string;
+              [x: `base-${string}`]: string;
+              [x: `layers-${string}`]: string;
+          }
+    ) = $props();
+
+    const footnotes = inputFootnotes.split(",").map((f) => f.trim());
+
+    const groups =
+        "groups" in props
+            ? props.groups.split(",").map((g) => {
+                  const [id, name] = g.trim().split(" ");
+                  return { id, name };
+              })
+            : undefined;
+
+    let activeGroupId = $state(groups?.[0].id);
 </script>
 
-<LayeredPhotos
-  {name}
-  {base}
-  layers={layers.split(',').map((layer) => {
-    const [name, src, legend] = layer.trim().split(' ')
-    return { name, src, legend }
-  })}
-  footnotes={footnotes.split(',').map((f) => f.trim())}
+<link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/tailwindcss-preflight@1.0.1/preflight.min.css"
+    crossorigin="anonymous"
 />
+
+<div class="container">
+    <div class="header"><h1>{name}</h1></div>
+
+    {#if "groups" in props}
+        {#key activeGroupId}
+            <LayeredPhotos
+                base={props[`base-${activeGroupId}`]}
+                layers={props[`layers-${activeGroupId}`]
+                    .split(",")
+                    .map((layer) => {
+                        const [name, src, legend] = layer.trim().split(" ");
+                        return { name, src, legend };
+                    })}
+            />
+        {/key}
+
+        <div class="controls">
+            {#each groups as group}
+                <button
+                    onclick={() => (activeGroupId = group.id)}
+                    class:active={activeGroupId === group.id}
+                    >{group.name}</button
+                >
+            {/each}
+        </div>
+    {:else}
+        <LayeredPhotos
+            base={props.base}
+            layers={props.layers.split(",").map((layer) => {
+                const [name, src, legend] = layer.trim().split(" ");
+                return { name, src, legend };
+            })}
+        />
+    {/if}
+
+    <div class="footer">
+        <div class="footnotes">
+            {#each footnotes as footnote}
+                <p>{footnote}</p>
+            {/each}
+        </div>
+        <img
+            src="https://projects.twreporter.org/twreporter/ddd/2025-0823-vote/assets/logo-black.png"
+            class="logo"
+            alt="報導者 The Reporter"
+        />
+    </div>
+</div>
+
+<style>
+    * {
+        --tr-text: #404040;
+        color: var(--tr-text);
+        font-family: "Roboto Slab", "Noto Sans TC", sans-serif;
+        text-align: left !important;
+    }
+
+    .container {
+        max-width: 730px;
+        position: relative;
+        padding: 8px 10px;
+        background: #f1f1f1;
+        --btn-size: 10px;
+        border-top: #e2e2e2 1px solid;
+        border-bottom: #e2e2e2 1px solid;
+    }
+
+    @media (min-width: 500px) {
+        .container {
+            padding: 10px 10px;
+        }
+    }
+
+    .header {
+        padding: 5px 0 0;
+    }
+
+    @media (min-width: 500px) {
+        .header h1 {
+            padding: 5px 0 15px;
+        }
+    }
+
+    .header h1 {
+        font-size: 24px;
+        font-weight: bold;
+        padding: 5px 0 10px;
+    }
+
+    @media (min-width: 500px) {
+        .header h1 {
+            font-size: 28px;
+        }
+    }
+
+    @media (min-width: 500px) {
+        .container {
+            --btn-size: 14px;
+        }
+    }
+
+    @media (min-width: 670px) {
+        .container {
+            --btn-size: 16px;
+        }
+    }
+
+    .controls {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .controls button {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 4px 0;
+        background: white;
+        border-radius: 0 0 5px 5px;
+        font-size: var(--btn-size);
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        opacity: 0.5;
+        letter-spacing: 0.7px;
+    }
+
+    .controls .active {
+        opacity: 1;
+        font-weight: 600;
+    }
+
+    .footer {
+        padding: 10px 0 10px 0;
+        display: flex;
+        align-items: end;
+        justify-content: space-between;
+        --footer-scale: 1;
+        --footer-logo-scale: 1.25;
+    }
+
+    @media (min-width: 500px) {
+        .footer {
+            padding: 15px 0 10px 0;
+            --footer-scale: 1.6;
+            --footer-logo-scale: 2;
+        }
+    }
+
+    .footnotes {
+        display: flex;
+        flex-direction: column;
+        gap: 2px;
+    }
+
+    @media (min-width: 500px) {
+        .footnotes {
+            gap: 5px;
+        }
+    }
+
+    .footer p {
+        color: #acacac;
+        font-size: calc(10px * var(--footer-scale));
+    }
+
+    .footer .logo {
+        width: calc(14.5px * var(--footer-logo-scale));
+        height: calc(15.5px * var(--footer-logo-scale));
+    }
+</style>
