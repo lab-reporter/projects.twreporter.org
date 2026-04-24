53359d2e6ce1ac29e4e85f7171acd8e0241f671e
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-04-24 15:35:27 +0800

    chore(2026-05-lawmaker): setup wordcloud list rendering

--- packages/twreporter/ddd/2026-05-lawmaker/src/WordCloud.wc.svelte
+++ packages/twreporter/ddd/2026-05-lawmaker/src/WordCloud.wc.svelte
@@ -3,6 +3,18 @@
 <script lang="ts">
   import WordCloud from './lib/components/WordCloud.svelte'
   import Shell from './lib/components/layout/Shell.svelte'
+
+  const wordclouds = [
+    {
+      name: '國民黨',
+      color: 'var(--supportive-heavy)',
+      src: 'https://storage.googleapis.com/data-reporter-infographics/dev/2026-05-lawmaker/data/test.json',
+    },
+    {
+      name: '民進黨',
+      src: 'https://storage.googleapis.com/data-reporter-infographics/dev/2026-05-lawmaker/data/test.json',
+    },
+  ]
 </script>
 
 <Shell
@@ -13,7 +25,47 @@
     '資料整理：黃靖緯 ｜ 設計：江世民',
   ]}
 >
-  <WordCloud
-    src="https://storage.googleapis.com/data-reporter-infographics/dev/2026-05-lawmaker/data/test.json"
-  />
+  <div class="container">
+    {#each wordclouds as wordcloud}
+      <div
+        class="block label"
+        style:--color="var(--supportive-heavy)"
+        style:--text-color="white"
+      >
+        {wordcloud.name}
+      </div>
+      <div class="block wordcloud">
+        <WordCloud ratio={2 / 1} src={wordcloud.src} />
+      </div>
+    {/each}
+  </div>
 </Shell>
+
+<style>
+  .container {
+    display: grid;
+    grid-template-columns: 40px 1fr;
+    gap: 5px;
+  }
+
+  .block {
+    border-radius: 5px;
+    background-color: var(--color, var(--neutral-gray-100));
+    color: var(--text-color, var(--neutral-gray-900));
+    padding: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    word-wrap: break-word;
+  }
+
+  .block.label {
+    text-align: center;
+    font-weight: 450;
+    font-size: var(--text-m);
+  }
+
+  .block.wordcloud {
+    padding: 10px;
+  }
+</style>
