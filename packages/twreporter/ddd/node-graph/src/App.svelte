ceb5713aba458092df17b4732f1cf5c80ee0b471
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-04-27 16:37:15 +0800

    feat(node-graph): setup clerk

--- packages/twreporter/ddd/node-graph/src/App.svelte
+++ packages/twreporter/ddd/node-graph/src/App.svelte
@@ -1,4 +1,20 @@
 <script lang="ts">
+    import {
+        ClerkProvider,
+        Show,
+        SignInButton,
+        UserButton,
+    } from 'svelte-clerk/client'
+
+    import { env } from './environment-variables'
 </script>
 
-<div>Hello, World!</div>
+<ClerkProvider publishableKey={env.clerk.publishableKey}>
+    <Show when="signed-out">
+        <SignInButton />
+    </Show>
+    <Show when="signed-in">
+        <UserButton />
+    </Show>
+    <div>Hello, World!</div>
+</ClerkProvider>
---
+++ packages/twreporter/ddd/node-graph/src/environment-variables.ts
@@ -0,0 +1,5 @@
+export const env = {
+  clerk: {
+    publishableKey: import.meta.env.VITE_CLERK_PUBLISHABLE_KEY,
+  },
+}
