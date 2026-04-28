f66037089f727b5c4c16a7a88211256637e05ef2
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-04-28 10:48:51 +0800

    chore(node-graph): configure vite base path

--- packages/twreporter/ddd/node-graph/vite.config.ts
+++ packages/twreporter/ddd/node-graph/vite.config.ts
@@ -3,5 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:
+    process.env.RELEASE === 'prod'
+      ? '/twreporter/ddd/node-graph/js'
+      : '/data-reporter-infographics/dev/node-graph/js',
   plugins: [svelte()],
 })
