840e2e5a294657e9106b580a278fe8272b349591
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-04-29 10:50:43 +0800

    feat(node-graph): patch sv-router path generator for hash-based href to work

--- packages/twreporter/ddd/node-graph/src/router.ts
+++ packages/twreporter/ddd/node-graph/src/router.ts
@@ -2,7 +2,16 @@ import { createRouter } from 'sv-router'
 import Home from './routes/Home.svelte'
 import Editor from './routes/Editor.svelte'
 
-export const { p, navigate, isActive, route } = createRouter({
+export const {
+  p: path,
+  navigate,
+  isActive,
+  route,
+} = createRouter({
   '/': Home,
   '/editor': Editor,
 })
+
+type P = typeof path
+
+export const p: P = (...args) => path(...args).replace(/^\//, '')
