649bb4292e82dfce380f7aa2c4804e7ffff735e7
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-04-29 18:34:33 +0800

    chore(node-graph): setup convex import alias

--- packages/twreporter/ddd/node-graph/vite.config.ts
+++ packages/twreporter/ddd/node-graph/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,6 +8,11 @@ export default defineConfig({
     process.env.RELEASE === 'prod'
       ? '/twreporter/ddd/node-graph/js'
       : '/data-reporter-infographics/dev/node-graph/js',
+  resolve: {
+    alias: {
+      '~convex': fileURLToPath(new URL('./convex/_generated', import.meta.url)),
+    },
+  },
   plugins: [
     svelte({
       emitCss: false,
