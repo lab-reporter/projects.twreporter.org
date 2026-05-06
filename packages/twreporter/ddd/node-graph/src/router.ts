ad4e12893c6ed7b5bbb9caf9cd61c4d9cf29f812
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-05-06 10:48:21 +0800

    chore(node-graph): organize routes

--- packages/twreporter/ddd/node-graph/src/router.ts
+++ packages/twreporter/ddd/node-graph/src/router.ts
@@ -1,6 +1,7 @@
 import { createRouter } from 'sv-router'
 import Home from './routes/Home.svelte'
-import Editor from './routes/Editor.svelte'
+import GraphEditor from './routes/GraphEditor.svelte'
+import DesignEditor from './routes/DesignEditor.svelte'
 
 export const {
   p: path,
@@ -9,8 +10,8 @@ export const {
   route,
 } = createRouter({
   '/': Home,
-  '/graphs/:graphId': Editor,
-  '/graphs/:graphId/designs/:designId': Editor,
+  '/graphs/:graphId': GraphEditor,
+  '/graphs/:graphId/designs/:designId': DesignEditor,
 })
 
 type P = typeof path
