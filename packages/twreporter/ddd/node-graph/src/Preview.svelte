ef812592b2dd31aa25dfebb7bc7335255a747881
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-05-11 15:26:03 +0800

    chore(node-graph): implement basic web component preview page

--- packages/twreporter/ddd/node-graph/src/Preview.svelte
+++ packages/twreporter/ddd/node-graph/src/Preview.svelte
@@ -1,1 +1,15 @@
-<div>preview page</div>
+<script lang="ts">
+    import Layout from './lib/components/Layout.svelte'
+
+    let inputHtml = $state()
+</script>
+
+<Layout>
+    <h1>預覽頁面</h1>
+
+    <textarea bind:value={inputHtml}></textarea>
+
+    <div>
+        {@html inputHtml}
+    </div>
+</Layout>
