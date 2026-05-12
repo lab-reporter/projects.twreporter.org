ed86013faf8b3318aa438fc0110ee55bb8cb7bc9
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-05-12 11:29:56 +0800

    feat(node-graph): install toaster

--- packages/twreporter/ddd/node-graph/src/App.svelte
+++ packages/twreporter/ddd/node-graph/src/App.svelte
@@ -2,18 +2,23 @@
     import { Router } from 'sv-router'
     import './router'
 
+    import { SvelteFlowProvider } from '@xyflow/svelte'
     import { setupConvex } from 'convex-svelte'
     import { ClerkProvider } from 'svelte-clerk/client'
 
     import AuthProvider from './lib/components/providers/AuthProvider.svelte'
     import { clientEnv } from './lib/environment-variables'
 
-    setupConvex(clientEnv.convex.url)
+    import { Toaster } from 'svelte-sonner'
 
+    setupConvex(clientEnv.convex.url)
 </script>
 
 <ClerkProvider publishableKey={clientEnv.clerk.publishableKey}>
     <AuthProvider>
-        <Router base="#" />
+        <SvelteFlowProvider>
+            <Router base="#" />
+            <Toaster />
+        </SvelteFlowProvider>
     </AuthProvider>
 </ClerkProvider>
