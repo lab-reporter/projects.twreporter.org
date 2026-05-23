8905a7740958fea0ed0f393ae66535a97180bf09
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-05-23 19:06:57 +0800

    fix(node-graph): align release env

--- packages/twreporter/ddd/node-graph/vite.config.ts
+++ packages/twreporter/ddd/node-graph/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   base:
-    process.env.RELEASE === 'prod'
+    process.env.RELEASE === 'production'
       ? '/twreporter/ddd/node-graph/js'
       : '/data-reporter-infographics/dev/node-graph/js',
   resolve: {
