0a0b31ae4001591363e755934462b1cf9b1ab1b2
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-05-24 13:10:48 +0800

    chore(node-graph): add csv import template url

--- packages/twreporter/ddd/node-graph/src/lib/constants/assets.ts
+++ packages/twreporter/ddd/node-graph/src/lib/constants/assets.ts
@@ -3,4 +3,6 @@ export const assets = {
   webComponentScript:
     'https://projects.twreporter.org/twreporter/ddd/node-graph/web-components/node-graph.js',
   embedCSS: 'https://projects.twreporter.org/twreporter/ddd/shared/embed.css',
+  csvImportTempate:
+    'https://docs.google.com/spreadsheets/d/1IUmgDqsLJeJqy9XOJaE5X_UQ-Lq4qYfYMCjZmS47XJU/edit?usp=sharing',
 }
