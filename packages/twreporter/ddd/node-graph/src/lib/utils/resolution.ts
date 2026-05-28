94214fdfe58fd755e3eb1763a19f30decf36cc7f
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-05-28 15:10:15 +0800

    fix(node-graph): add function overload to fix typings

--- packages/twreporter/ddd/node-graph/src/lib/utils/resolution.ts
+++ packages/twreporter/ddd/node-graph/src/lib/utils/resolution.ts
@@ -4,7 +4,14 @@ export const gcd = (a: number, b: number): number => {
   return b ? gcd(b, a % b) : a
 }
 
-export function resolutionToRatio([width, height]: Resolution) {
+export function resolutionToRatio(resolution: null): null
+export function resolutionToRatio(
+  resolution: NonNullable<Resolution>,
+): NonNullable<Resolution>
+export function resolutionToRatio(resolution: Resolution) {
+  if (!resolution) return null
+
+  const [width, height] = resolution
   const divisor = gcd(width, height)
 
   return [width / divisor, height / divisor]
